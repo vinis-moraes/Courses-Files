c9cd24ab6fb3c6148683b8c6034606b0739495bb
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-12-19 20:19:34 -0300

    Add "Python Syntax" and "Python Variables"

---
+++ python/01-python_syntax.py
@@ -0,0 +1,8 @@
+# Python Indentation
+if 5 > 2:
+    print("Five is greater than two.")
+
+# Variables
+
+x = 5
+y = "Hello, World!"
---
+++ python/02-python_variables.py
@@ -0,0 +1,21 @@
+# Assign a value to variable
+x = 5
+y = "John"
+# Print variables
+print(x)
+print(y)
+# You can change the type of the variable after declaring it
+z = 3
+print("z = {}" .format(z))
+z = "Sally"
+print("now, z = {}".format(z))
+# Casting
+a = str("3")
+b = int(3)
+c = float(3)
+print("{}, {}, {:.2f}".format(a, b, c))
+# Types of variables
+print(type(x))
+print(type(y))
+# There is no difference between using single or double quote marks on strings;
+# Variables are case-sensitive: assign a value to A will not overwrite a
