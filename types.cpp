75cc96bd4e7a484d22f2e1e640457dd31660fad5
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-12-19 14:02:08 -0300

    Add "Check Version" and "Types"

---
+++ types.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void){
+    int num = 42;
+    printf("%i\n", num);
+    float pi = 3.14;
+    printf("%.2f\n", pi);
+    double hi_precision_pi = 3.1415926535;
+    printf("%.10f\n", hi_precision_pi);
+    char letter = 'A';
+    printf("%c\n", letter);
+}
