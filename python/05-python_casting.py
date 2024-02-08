d26d96b2326d6be7f2a650ae65d55a2355b24695
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2024-02-08 10:14:45 -0300

    Create 05-python_casting.py

---
+++ python/05-python_casting.py
@@ -0,0 +1,8 @@
+print("### Casting types in Python ###")
+
+x = int(1) # Integer "x" gets 1
+print(x, type(x))
+y = float(3.5) # Float "y" gets 3.5
+print(y, type(y))
+z = str(2) # String "z" gets 2
+print(z, type(z))
