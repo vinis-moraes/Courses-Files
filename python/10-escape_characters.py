fa0577f53cdf8c5e1f6db207d8ca4e8af8a8a907
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2024-02-08 13:59:09 -0300

    Create 10-escape_characters.py

---
+++ python/10-escape_characters.py
@@ -0,0 +1,17 @@
+print("This is the way to use \"double-quotes\" on Python!")
+
+print("Creating a new line \n")
+
+print("Using Backslash \\")
+
+print("Adding \ra new line to the text")
+
+print("\t This is a tab")
+
+print("\bAnd this is a backspace ")
+
+print("\fForm something...?")
+
+print("This allows Octadecimal values \101")
+
+print("And this allows Hexadecimal values: \x41")
