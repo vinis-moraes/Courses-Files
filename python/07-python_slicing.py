a7738fcba59e0df91e7288b22c7baff715d24af5
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2024-02-08 13:40:39 -0300

    Update 07-python_slicing.py

---
+++ python/07-python_slicing.py
@@ -0,0 +1,8 @@
+b = "Hello, World!"
+print("The letters from Third to Sixth positions are \"{}\"".format(b[2:6]))
+
+print("The letters ultil position 5 are \"{}\"".format(b[:5]))
+
+print("The letters from position 2 are \"{}\"".format(b[2:]))
+
+print("The letters from the fifth position (starting from the end) until the second (stating from the end) are \"{}\"".format(b[-5: -2]))
